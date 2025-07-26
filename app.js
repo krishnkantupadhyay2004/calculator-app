1e3d826907858fc1cb0883127b11161136920107
krishnkantupadhyay2004 <krishnkant.sjp2004@gmail.com>
2025-07-26 22:42:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+let inputtext = document.querySelector(".inputtext");
+let outputtext = document.querySelector(".outputtext");
+let firsttime = true;
+let str = "";
+let mode = "light";
+let body = document.querySelector("body");
+let model = document.querySelector(".model");
+let moon = document.querySelector(".moonicon");
+let bright = document.querySelector("#bright");
+let numberbutton = document.querySelectorAll(".numberbutton");
+let signbtn = document.querySelectorAll(".signbtn");
+
+
+
+function addnumber(number){
+    if(firsttime || number == '000'){
+        console.log(number);
+        if(number=='000'){
+            firsttime = true;
+            inputtext.innerHTML = "0";
+            outputtext.innerHTML = "0";
+            str = "";
+        } else{
+             firsttime = false;
+             inputtext.innerHTML = number;
+             str += number;
+        } 
+    }else{
+        inputtext.innerHTML += number;
+        str += number;
+    }
+
+    console.log("str=",str);
+}
+
+function answer(){
+    if(str[0] == '/' || str[0] == '*'){
+        outputtext.innerHTML = "sahi no. daalBSDK";
+    }else {
+
+    let result = eval(str);
+    outputtext.innerHTML = result;
+    }
+}
+
+
+
+function changemode(){
+    document.body.classList.toggle('dark-mode');
+}
